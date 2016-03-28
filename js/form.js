3a435b2f46446d0afa5de9adc15b5f080f96acd9
c.g. Acharjya <captain@stratagemstudios.com>
2016-03-27 21:25:10 -0700

    writing if else logic

--- js/form.js
+++ js/form.js
@@ -11,21 +11,7 @@ console.log("whatever " + slt_weather_val);
 }
 
 
-//  event.preventDefault();
-
-
-      // this is what I wanrt to write the js to capture <form id="personalinfo">
-//  <form>
-//                         <div class="form-group">
-
-//                             <p><label for="vacapref">Do you prefer Lots of Rain, Indoor Activities or Stormy Seas?</label>
-//                                 <select select name="weather" id='slt_weather'>>
-//                                     <option value="rain">Rainy all the way!</option>
-//                                     <option value="indoors">What is this Outside you talk of?</option>
-//                                     <option value="sea">Stormy Seas, Matey</option>
-//                                 </select></p>
-//                         </div>
-//                         nothing else needs to work or take in data. just that first selection.
+if 
 
 // then i need to show with an if else the tags 
 // #portland, #storm, #catacombs {
