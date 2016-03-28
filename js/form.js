f10507af60d5f6268dea946cff8691e5f5cd2ad1
c.g. Acharjya <captain@stratagemstudios.com>
2016-03-27 22:05:10 -0700

    last commit here.
    no idea why the if else if isn’t working. but this same syntax works in
    your examples, on line and elsewhere.

--- js/form.js
+++ js/form.js
@@ -8,20 +8,25 @@ var slt_weather_val=$("#slt_weather").val();
   });
 }
 
+// I know I'm getting data back here but the if else is broken. if I take out the ; it complains that it wants one, if I put it in, it complains about the else if.
 
-if ($('#slt_weather_val').val() === 'rain'){
+if ($("#submit").val() === '#rain'); {
  alert("Portland has everything you need!");
-}
-if ($('#slt_weather_val').val() === 'sea'){
+} else if ($('#slt_weather_val').val() === '#sea'); {
   alert("Storm Cove Will sooth your melancholy soul.");
-}
-if ($('#slt_weather_val').val() === 'catacombs'){
+} else ($('#slt_weather_val').val() === '#catacombs');{
   alert("You should go to the Catacombs Of Paris.");
 }
 
+// Your example:
+// if (age > 21) {
+//   $('#drinks').show();
+// } else if (age === 21) {
+//   $('#drinks').show();
+// } else {
+//   $('#under-21').show();
+// }
 
-
-  // if($('#arrivalTimeCheck:checked').val() === 'on')
 
 // then i need to show with an if else the tags 
 // #portland, #storm, #catacombs {
