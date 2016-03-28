9ee4cf65368d2143cf162cf2da7f92786f2239be
c.g. Acharjya <captain@stratagemstudios.com>
2016-03-27 21:43:06 -0700

    more if else loops. adding response logic

--- js/form.js
+++ js/form.js
@@ -5,13 +5,23 @@ $(document).ready(main);
 {
   $("#submit").click(function(event){
 var slt_weather_val=$("#slt_weather").val();
-console.log("whatever " + slt_weather_val);
-  }); 
-
+  });
 }
 
 
-if 
+if ($('#slt_weather_val').val() === 'rain'){
+ alert("Portland has everything you need!");
+}
+if ($('#slt_weather_val').val() === 'sea'){
+  alert("Storm Cove Will sooth your melancholy soul.");
+}
+if ($('#slt_weather_val').val() === 'catacombs'){
+  alert("You should go to the Catacombs Of Paris.");
+}
+
+
+
+  // if($('#arrivalTimeCheck:checked').val() === 'on')
 
 // then i need to show with an if else the tags 
 // #portland, #storm, #catacombs {
