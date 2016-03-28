5c14c237b1eaf16784e93e02ebc6e0f3db7fca31
c.g. Acharjya <captain@stratagemstudios.com>
2016-03-27 20:38:17 -0700

    cleanup of code
    added more bootstrap to clean up the form, put new js format in place
    to work on rewriting it again.

---
+++ js/form.js
@@ -0,0 +1,29 @@
+$(document).ready(function() 
+{
+  $("#submit").click(function(event) 
+
+}
+
+ event.preventDefault();
+
+
+      // this is what I wanrt to write the js to capture <form id="personalinfo">
+//  <form>
+//                         <div class="form-group">
+//                             <p><label for="vacapref">Do you prefer Lots of Rain, Indoor Activities or Stormy Seas?</label>
+//                                 <select select name="weather" id='slt_weather'>>
+//                                     <option value="rain">Rainy all the way!</option>
+//                                     <option value="indoors">What is this Outside you talk of?</option>
+//                                     <option value="sea">Stormy Seas, Matey</option>
+//                                 </select></p>
+//                         </div>
+//                         nothing else needs to work or take in data. just that first selection.
+
+// then i need to show with an if else the tags 
+// #portland, #storm, #catacombs {
+// display: none;
+
+//     }
+
+
+  });
