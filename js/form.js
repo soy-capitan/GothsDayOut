1a38d503736db356458c60d4813c70e59a8b13b7
c.g. Acharjya <captain@stratagemstudios.com>
2016-03-27 21:16:15 -0700

    added correct listener
    now I’m getting data yay!

--- js/form.js
+++ js/form.js
@@ -1,15 +1,23 @@
-$(document).ready(function() 
+$(document).ready(main);
+ 
+ function main() 
+
 {
-  $("#submit").click(function(event) 
+  $("#submit").click(function(event){
+var slt_weather_val=$("#slt_weather").val();
+console.log("whatever " + slt_weather_val);
+  }); 
 
 }
 
- event.preventDefault();
+
+//  event.preventDefault();
 
 
       // this is what I wanrt to write the js to capture <form id="personalinfo">
 //  <form>
 //                         <div class="form-group">
+
 //                             <p><label for="vacapref">Do you prefer Lots of Rain, Indoor Activities or Stormy Seas?</label>
 //                                 <select select name="weather" id='slt_weather'>>
 //                                     <option value="rain">Rainy all the way!</option>
@@ -25,5 +33,3 @@ $(document).ready(function()
 
 //     }
 
-
-  });
