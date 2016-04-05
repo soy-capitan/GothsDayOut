88c01957a5aba2c4103bb710d1dfa4c034710266
c.g. Acharjya <soy-capitan@users.noreply.github.com>
2016-04-05 13:43:09 -0700

    update brackets.

--- js/form.js
+++ js/form.js
@@ -9,11 +9,11 @@ var humor_val=$("#humor").val();
 
 
 //added a couple options to show I understand the concept of each form of logic. This could be expanded using this same logic/form though at that point it's just typing. I could refactor this with new stuff, but that isn't in the scope of this assignment.
-if (slt_weather_val === 'rain') && (humor_val === 'fantasy' || 'dead') {
+if ((slt_weather_val === 'rain') && (humor_val === 'fantasy' || humor_val === 'dead')) {
  alert("Portland has everything you need!");
-} else if (slt_weather_val === 'sea') && (humor_val === 'fantasy') {
+} else if ((slt_weather_val === 'sea') && (humor_val === 'fantasy')) {
   alert("Storm Cove Will sooth your melancholy soul.");
-} else if (slt_weather_val === 'sea') && (humor_val === 'dead') {
+} else if ((slt_weather_val === 'sea') && (humor_val === 'dead')) {
   alert("Set your sails for The Burmuda Triangle.");
 } else if (slt_weather_val === 'indoors') {
   alert("You should go to the Catacombs Of Paris.");
