fec7f90708deea5651b3f80b0c948957c966f3af
c.g. Acharjya <captain@stratagemstudios.com>
2016-04-07 16:04:09 -0700

    added show
    as required by assignment

--- js/form.js
+++ js/form.js
@@ -1,20 +1,27 @@
 $(document).ready(main);
   function main() {
+      
+
     $("form").submit(function(event){
+
+
       var slt_weather_val=$("#slt_weather").val();
       var humor_val=$("#humor").val();
            //added a couple options to show I understand the concept of each form of logic. 
           // This could be expanded using this same logic/form though at that point it's just typing. 
           // I could refactor this with new stuff, but that isn't in the scope of this assignment.
       if ((slt_weather_val === 'rain') && (humor_val === 'fantasy' || humor_val === 'dead')) {
-        alert("Portland has everything you need!");} 
+        alert("Portland has everything you need!");
+        $("#retry").show();} 
       else if ((slt_weather_val === 'sea') && (humor_val === 'fantasy')) {
-        alert("Storm Cove Will sooth your melancholy soul.");} 
+        alert("Storm Cove Will sooth your melancholy soul.");
+        $("#retry").show();} 
       else if ((slt_weather_val === 'sea') && (humor_val === 'dead')) {
         alert("Set your sails for The Burmuda Triangle.");}   
       else if (slt_weather_val === 'indoors') {
         alert("You should go to the Catacombs Of Paris.");
-    }
+        $("#retry").show();
+      }
     event.preventDefault();
   });
 }
